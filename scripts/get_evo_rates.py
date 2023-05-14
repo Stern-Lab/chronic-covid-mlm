7ae730e388c0e39c621b7a081669449f4c4d5a95
daniellemiller <danimillers10@gmail.com>
2023-05-14 18:55:36 +0300

    minor bug fix in stratified sampling

--- scripts/get_evo_rates.py
+++ scripts/get_evo_rates.py
@@ -9,8 +9,11 @@ def stratify_by_clade_and_size(df, dist, col):
     res = []
     for k, v in dist.items():
         cur = df[df['clade'] == k[0]]
+        if cur.shape[0] == 0:
+            continue
         cur = cur[(cur['size'] >= k[1] - 2) & (cur['size'] <= k[1] + 2)]
-
+        if cur.shape[0] == 0:
+            continue
         r = cur.sample(n=v, replace=True)[col].tolist()
         res.extend(r)
     return res
@@ -29,8 +32,8 @@ def main(args):
     gene = args.gene
     input_dir = args.input
 
-    cases = pd.read_csv(os.path.join(input_dir, f"cases_group_data_{mode}_rates_nsp.csv"))
-    controls = pd.read_csv(os.path.join(input_dir, f"controls_group_data_{mode}_rates_nsp.csv"))
+    cases = pd.read_csv(os.path.join(input_dir, f"cases_group_data_{mode}_rates.csv"))
+    controls = pd.read_csv(os.path.join(input_dir, f"controls_group_data_{mode}_rates.csv"))
 
     clades = dict(cases.groupby(['clade', 'size']).size())
     res = bootstrap(controls, clades, col=gene, m=m)
@@ -51,7 +54,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Rate bootstrap')
-    parser.add_argument('--input', default='/sternadi/home/volume3/chronic-corona-pred/data/case_control/rates/', type=str,
+    parser.add_argument('--input', default='/sternadi/home/volume3/chronic-corona-pred/data/case_control/rates/no_sp/', type=str,
                         help='input directory for rate estimation')
     parser.add_argument('--gene', default='ORF1a',help='gene name')
     parser.add_argument('--mode', default='overall', type=str, help='overall, syn or non_syn modes')
